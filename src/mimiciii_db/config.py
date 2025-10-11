a3783140e052b60e20b12dac968981a190f8bf93
Guoxuan Xu <g7xu@ucsd.edu>
2025-10-11 16:34:40 -0700

    Update config.py

--- src/mimiciii_db/config.py
+++ src/mimiciii_db/config.py
@@ -1,5 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 
 def db_url(env_var: str = "DATABASE_URL") -> str:
     url = os.getenv(env_var)
